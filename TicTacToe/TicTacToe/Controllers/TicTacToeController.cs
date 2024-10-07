0352bdaed4e177c6c515be9f0466698bdb68dd82
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-10-07 14:56:26 +0200

    Working on the controller

---
+++ TicTacToe/TicTacToe/Controllers/TicTacToeController.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using TicTacToe.GameLogic;
+
+namespace TicTacToe.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class TicTacToeController: ControllerBase
+{
+    //Creating a static instance of the game
+    private static TicTacToeGame game = new TicTacToeGame();
+
+    //get the current state of the game board 
+    [HttpGet("board")]
+    public IActionResult GetBoard()
+    {
+        var boardString = new StringBuilder();
+        for (int i = 0; i < game.Board.GetLength(0); i++)
+        {
+            for (int j = 0; j < game.Board.GetLength(1); j++)
+            {
+                boardString.Append(game.Board[i, j] + " ");
+            }
+            boardString.AppendLine();
+        }
+        
+        //returns the board as a string
+        return Ok(boardString.ToString());
+    }
+    
+}
