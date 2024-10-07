e78b6cfe98f9e446bc753e876d66ffff42be0494
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-10-07 14:01:48 +0200

    Update to game logic

--- TicTacToe/TicTacToe/GameLogic/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/GameLogic/TicTacToeGame.cs
@@ -2,5 +2,13 @@
 
 public class TicTacToeGame
 {
-    
+    public char [,] Board { get; set; }
+    public char [,] CurrentPlayer { get; set; }
+    public bool isGameOver { get; set; }
+    public string winner { get; set; }
+
+    public TicTacToeGame()
+    {
+        
+    }
 }
