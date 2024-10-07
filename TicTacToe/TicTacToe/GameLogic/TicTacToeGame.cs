2cdebdcac39f0815632022fb7aea3ca329cd1873
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-10-07 14:11:24 +0200

    created a Initialize method for my game board

--- TicTacToe/TicTacToe/GameLogic/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/GameLogic/TicTacToeGame.cs
@@ -3,12 +3,26 @@
 public class TicTacToeGame
 {
     public char [,] Board { get; set; }
-    public char [,] CurrentPlayer { get; set; }
+    public char CurrentPlayer { get; set; }
     public bool isGameOver { get; set; }
     public string winner { get; set; }
 
     public TicTacToeGame()
     {
-        
+        Board = new char[3, 3]; //creates the game
+        InitializeBoard();
+        CurrentPlayer = 'X'; // X-player always starts first
+        isGameOver = false;
+    }
+
+    private void InitializeBoard()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Board[i, j] = '-'; //set each positions to '-'
+            }
+        }
     }
 }
